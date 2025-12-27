7b721a1afe697b7da65c0ce98a17313a8b2fa424
chakkrit <chakkrit@miracle.co.th>
2025-12-27 09:25:50 +0700

    fix: remove existing valid_detail type

--- supabase/migrations/20251227015903_remote_schema.sql
+++ supabase/migrations/20251227015903_remote_schema.sql
@@ -17820,7 +17820,7 @@ create or replace view "public"."v_user_favorite_stores" as  SELECT o.customer_i
   ORDER BY o.customer_id, (count(o.id)) DESC, (max(o.created_at)) DESC;
 
 
-create type "public"."valid_detail" as ("valid" boolean, "reason" character varying, "location" public.geometry);
+-- create type "public"."valid_detail" as ("valid" boolean, "reason" character varying, "location" public.geometry);
 
 CREATE OR REPLACE FUNCTION public.validate_discount_code(p_code text, p_store_id uuid, p_subtotal numeric)
  RETURNS jsonb
