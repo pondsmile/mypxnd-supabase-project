de070334fffaa9ea133c544e17404a6267c4635b
chakkrit <chakkrit@miracle.co.th>
2025-12-27 19:33:07 +0700

    chore: Remove commented-out `geometry_dump` type creation.

--- supabase/migrations/20251227015903_remote_schema.sql
+++ supabase/migrations/20251227015903_remote_schema.sql
@@ -9510,8 +9510,6 @@ END;
 $function$
 ;
 
--- create type "public"."geometry_dump" as ("path" integer[], "geom" public.geometry);
-
 CREATE OR REPLACE FUNCTION public.get_active_beauty_salons()
  RETURNS TABLE(id uuid, salon_name text, description text, address text, phone text, latitude numeric, longitude numeric, rating numeric, total_reviews integer)
  LANGUAGE plpgsql
